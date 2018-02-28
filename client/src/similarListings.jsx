765e6fcc63bedd93dafb87829b0511c13a66caef
Nyah Check <check.nyah@gmail.com>
2018-02-28 00:59:51 -0800

    Add similarListings jsx and styles.

--- client/src/similarListings.jsx
+++ client/src/similarListings.jsx
@@ -1,35 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Slider from "react-slick";
-import starrating from 'react-star-rating-component';
+import Rating from 'react-star-rating-component';
+import * as vibrant from 'node-vibrant';
 import axios from 'axios';
-import $ from 'jquery';
 
 
-class ReactSlickDemo extends React.Component {
-  render() {
-    var settings = {
-      dots: true
-    };
+class SimilarListings extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      similarListing: {}
+    }
+
+  } 
+
+  componentDidMount() {
+    vibrant.from(this.props.similarListing.photo.data).getPalette((err, palette) => console.log(palette))
+  }
+
+  render () {
     return (
-      <div className="container">
-        <Slider {...settings}>
-          <div>
-            <img src="http://placekitten.com/g/400/200" />
-          </div>
-          <div>
-            <img src="http://placekitten.com/g/400/200" />
-          </div>
-          <div>
-            <img src="http://placekitten.com/g/400/200" />
-          </div>
-          <div>
-            <img src="http://placekitten.com/g/400/200" />
-          </div>
-        </Slider>
+      <div 
+        onClick={() => {this.props.handleClick(this.props.currentListing.id)}}
+        className="similarListing_container">
+          <div className="similarListing_inner_container">
+            <img className="similarListing_img" src={this.props.currentListing.photo.data} width="237" height="158" />
+            <div className="similarListing_category"><span>{this.props.currentListing.desc}</span> · <span>{this.props.currentListing.city}</span></div>
+            <div className="similarListing_title">{this.props.currentListing.address}</div>
+            <div className="similarListing_price">
+              $<span>{this.props.currentListing.price}
+              </span> per night
+            </div>
+            <div className="similarListing_rating">
+                <Rating
+                  name={'ratingStars'}
+                  value={this.props.currentListing.ratings}
+                  editing={false}
+                  starColor={'#008489'}
+                  emptyStarColor={'#D8D8D8'}
+                />
+            <div className="similarListing_rating">{this.props.currentListing.ratings}</div>
+            </div>
+        </div>
       </div>
     );
   }
 }
 
-ReactDOM.render(<ReactSlickDemo />, document.getElementById("container"));
+export default SimilarListings;
