59f1c97c160ece41d7dfa54f33c681cc7467670e
Nyah Check <check.nyah@gmail.com>
2018-02-27 23:51:23 -0800

    Add similar listings UI.

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,44 +1,86 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import Slider from "react-slick";
+import starrating from 'react-star-rating-component';
 import axios from 'axios';
+import SimilarListings from './similarListings.jsx';
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      listings: []
+    this.state = {
+      currentListing: {}, 
+      similarlistings: []
     }
+    this.fetch = this.fetch.bind(this);
+    this.fetchRatings = this.fetchRatings.bind(this);
+    this.getSimilarListings = this.getSimilarListings(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  getSimilarRooms (roomId) {
-    console.log(`Find rooms similar to ${roomId}`);
-  
-    axios.post('/rooms/similarListings', {
-      id: roomId
-    })
+  componentDidMount() {
+    this.getSimilarListings('San Francisco');
+  }
+
+  fetch(id) {
+    axios.get(`/rooms/:${id}/similarListings`)
     .then((res) => {
-      console.log('REACT RESP: ', res);
+      console.log('Success', res.data);
+      this.setState({currentListing: res.data[0], similarListings: res.data});
     })
     .catch((err) => {
-      console.log('Error: ', err);
-    });
-    
-    //Do a GET request:
-    axios.get('/rooms/similarListings')
-    .then (res => {
-      this.state.listings = res.data;
-      console.log('Repo SET', this.state.listings);
-    })
-    .catch(err => {
-      console.log(err);
+      console.error('Error fetching similar listings', err);
     });
   }
 
-  render () {
+  fetchRatings(id) {
+    axios.get(`/rooms/:${id}/similarListings`)
+     .then((res) => {
+      console.log('Success', res.data);
+    })
+    .catch((err) => {
+      console.error('Error getting ratings', err);
+    });
+  }
+
+  handleClick (id) {
+    this.fetch(id);
+  }
+
+
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToScroll: 4,
+      slidesToShow: 4
+    };
     return (
       <div>
-        <h1>HackBnB</h1>
+        <h2>HackBnB</h2>
+          <div className="similarListings">
+            <div className="similarListingsTitle">
+              <h1>Other highly rated Experiences <span>{this.state.currentListing.city}</span></h1>
+            </div>
+            <div className="similarExp">
+              {this.state.similarListings.length > 0 && <Slider {...settings}>
+                {this.state.similarListings.map((item, index) => {
+                  return  
+                    <div key={index}>
+                      <SimilarListings 
+                        handleClick={this.handleClick}
+                        similarListing={item} 
+                        currentListing={this.state.currentListing}
+                      />
+                    </div>
+                  })
+                }
+                </Slider>
+              }
+            </div>
+          </div>
       </div>
     );
   }
