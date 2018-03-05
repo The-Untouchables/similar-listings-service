c5d45c42feff8112f337abd225a992c07ef1cfd2
Nyah Check <check.nyah@gmail.com>
2018-03-05 12:31:28 -0800

    properly refactored react UI to dynamically render list. Issues occur in slider.

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -13,9 +13,63 @@ class App extends React.Component {
     super(props);
     this.state = {
       similarListings: [
-        
-
-
+       { photo: 
+           { data: '../images/pexels-photo-259580.jpeg',
+             contentType: 'image/jpg' },
+          id: 20828195,
+          address: 'Houston, TX, United States',
+          city: 'Houston',
+          desc: '' },
+        { photo: 
+           { data: '../images/pexels-photo-276625.jpeg',
+             contentType: 'image/jpg' },
+          id: 17012402,
+          address: 'Houston, TX, United States',
+          city: 'Houston',
+          desc: '' },
+        { photo: 
+           { data: '../images/pexels-photo-189333.jpeg',
+             contentType: 'image/jpg' },
+          id: 15445591,
+          address: 'Houston, TX, United States',
+          city: 'Houston',
+          desc: 'Traveler and Host\r\n' },
+        { photo: 
+           { data: '../images/pexels-photo-271654.jpeg',
+             contentType: 'image/jpg' },
+          id: 22744156,
+          address: 'Houston, TX, United States',
+          city: 'Houston',
+          desc: '' },
+        { photo: 
+           { data: '../images/pexels-photo-276663.jpeg',
+             contentType: 'image/jpg' },
+          id: 14029654,
+          address: 'Houston, TX, United States',
+          city: 'Houston',
+          desc: '' },
+        { photo: 
+           { data: '../images/pexels-photo-271654.jpeg',
+             contentType: 'image/jpg' },
+          id: 21414157,
+          address: 'Houston, TX, United States',
+          city: 'Houston',
+          desc: 'Hello, \r\n\r\nMy name is TJ. My Fiancee, Maria and I love to travel and are extremely passionate about making sure that your trip is everything you\'ve dreamt of and more. \r\n\r\nWhen you book with us, you have truly found "Your home away from home."' },
+        { photo: 
+           { data: '../images/pexels-photo-276663.jpeg',
+             contentType: 'image/jpg' },
+          id: 19035681,
+          address: 'Houston, TX, United States',
+          city: 'Houston',
+          desc: 'Born and raised in Houston, Texas very familiar with the area. I\'m a club promoter' },
+        { photo: 
+           { data: '../images/pexels-photo-210552.jpeg',
+             contentType: 'image/jpg' },
+          id: 15375322,
+          address: 'Houston, TX, United States',
+          city: 'Houston',
+          desc: '' 
+        }
       ]
     }
     this.fetch = this.fetch.bind(this);
@@ -46,7 +100,6 @@ class App extends React.Component {
   }
 
   render() {
-    this.fetch('20799308');
     const settings =  {
       dots: true,
       infinite: false,
@@ -87,7 +140,7 @@ class App extends React.Component {
           </div>
           <div className="similarExp">
             <Slider {...settings}>
-          		{this.state.similarListings.map((listing, i) => <SimilarListings key={i} value={listing}/>)}
+          		{this.state.similarListings.map((listing, index) => <SimilarListings key={index} value={listing}/>)}
             </Slider>
           </div>
         </div>
--- client/src/similarListings.jsx
+++ client/src/similarListings.jsx
@@ -7,24 +7,25 @@ import axios from 'axios';
 
 
 function SimilarListings (props) {
+  console.log('Props Key: ', props.value.id);
   return (
-    <div className="similarListing_container" onClick={() => {this.props.value.handleClick(this.props.key)}}>
+    <div className="similarListing_container" onClick={() => {props.value.handleClick(props.value.id)}}>
       <div className="similarListing_inner_container">
-        <img className="similarListing_img" src={this.props.value.photo.data} width="237" height="158" />
+        <img className="similarListing_img" src={props.value.photo.data} width="237" height="158" />
         <div className="similarListing_category">
-          <span>{this.props.similarListing.desc}</span> · <span>{this.props.value.city}</span>
+          <span>{props.value.desc}</span> · <span>{props.value.city}</span>
         </div>
-        <div className="similarListing_title">{this.props.value.address}</div>
-        <div className="similarListing_price">$<span>{this.props.value.price}</span> per night</div>
+        <div className="similarListing_title">{props.value.address}</div>
+        <div className="similarListing_price">$<span>{props.value.price}</span> per night</div>
         <div className="similarListing_rating">
           <Rating
             name={'ratingStars'}
-            value={this.props.value.ratings}
+            value={props.value.ratings}
             editing={false}
             starColor={'#008489'}
             emptyStarColor={'#D8D8D8'}
           />
-          <div className="similarListing_rating">{this.props.value.ratings}</div>
+          <div className="similarListing_rating">{props.value.ratings}</div>
         </div>
       </div>
     </div>
