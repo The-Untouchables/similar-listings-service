b3fb08795b70fa6811ad6fde16c60b2a3a9c6d21
Nyah Check <check.nyah@gmail.com>
2018-03-06 14:23:31 -0800

    Add display image styles and like button.

--- client/src/similarListings.jsx
+++ client/src/similarListings.jsx
@@ -1,31 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Slider from "react-slick";
 import Rating from 'react-star-rating-component';
-import * as vibrant from 'node-vibrant';
 import axios from 'axios';
 
 
 function SimilarListings (props) {
   return (
     <div className="similarListing_container">
-      <div className="similarListing_inner_container">
-        <img className="similarListing_img" src={props.value.photo.data} width="237" height="158" />
-        <div className="similarListing_category">
-          <span>ENTIRE APARTMENT</span> · <span>{props.value.beds} BEDS</span>
+      <div className="similarListing_img">
+        <div className="like">
+          <button type='button' class='button' aria-busy='false'>
+            <svg viewBox='0 0 32 32' fill='#484848' fill-opacity='0.5' stroke='#ffffff' stroke-width='2.5' aria-label='Add listing to a list' role='img' stroke-linecap='round' stroke-linejoin='round' style='height: 24px; width: 24px display: block;'></svg>
+          </button>
         </div>
-        <div className="similarListing_title">{props.value.desc}</div>
-        <div className="similarListing_price">$<span>{props.value.price}</span> per night</div>
-        <div>
-          <Rating
-            name={'ratingStars'}
-            value={props.value.stars}
-            editing={false}
-            starColor={'#484848'}
-            emptyStarColor={'#D8D8D8'}
-          />
-          <div className="similarListing_rating">{props.value.ratings}</div>
+        <div className='image'>
+          <img className="img" src={props.value.photo.data} width="237"/>
         </div>
+      </div>
+      <div className="similarListing_category">
+        <span>ENTIRE APARTMENT</span> · <span>{props.value.beds} BEDS</span>
+      </div>
+      <div className="similarListing_title">{props.value.desc}</div>
+      <div className="similarListing_price">$<span>{props.value.price}</span> per night</div>
+      <div>
+        <Rating
+          name={'ratingStars'}
+          value={props.value.stars}
+          editing={false}
+          starColor={'#484848'}
+          emptyStarColor={'#D8D8D8'}
+        />
+        <div className="similarListing_rating">{props.value.ratings}</div>
       </div>
     </div>
   );
