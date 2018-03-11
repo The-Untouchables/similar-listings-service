f2b67da6ed75341c9e7c2b7cdc5f9efafc0112ee
Nyah Check <check.nyah@gmail.com>
2018-03-10 17:40:23 -0800

    Add control headers to request backend.

--- server/index.js
+++ server/index.js
@@ -5,7 +5,11 @@ const db = require('../db/index');
 let app = express();
 let port = 3007;
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.static(__dirname + '/../client/dist'));
 app.use('/rooms/images', express.static(path.join(__dirname, '../images')));
 app.use(bodyParser.json());
